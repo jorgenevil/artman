3c14952b7a1119d6021504a1ddc85bbf36c37847
Jørgen Evil Ekvoll <jorgenekvoll@gmail.com>
2025-01-19 17:31:24 +0000

    started setting up project on my pad

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express from "express"
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.status(200)
+    res.json({message: "Hello world"})
+})
+
+app.listen(port, () => {
+    console.log('server listening on port', port)
+})
